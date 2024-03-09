25492850f79773651cb6811adc21d51cf5d76382
senna-lang <asrsena3302@gmail.com>
2024-03-09 18:34:09 +0900

    レイアウト調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
     <div>
       <div className="h-screen items-center justify-center bg-slate-300 md:flex">
         <div className="h-full md:flex xl:w-[1280px]">
-          <div className="h-full border-r md:w-1/4">
+          <div className="max-h-full border-r md:w-1/4">
             <SideBar />
           </div>
           <div className="max-h-screen md:w-3/4">
