b729edcb0489dea1807f61c47fad18ed127bc469
senna-lang <asrsena3302@gmail.com>
2024-03-09 18:34:56 +0900

    編集ボタンのフラグを追加

--- src/common/store/store.ts
+++ src/common/store/store.ts
@@ -1,15 +1,24 @@
-import { type } from 'os';
 import { create } from 'zustand';
 
 type State = {
   textId: number;
+  sidebarEdit: boolean;
+  mainAreaEdit: boolean;
 };
 
 type Action = {
   updateTextId: (textId: State['textId']) => void;
+  toggleSidebarEdit: (sidebarEdit: State['sidebarEdit']) => void;
+  toggleMainAreaEdit: (mainAreaEdit: State['mainAreaEdit']) => void;
 };
 
 export const useStore = create<Action & State>(set => ({
   textId: 1,
+  sidebarEdit: false,
+  mainAreaEdit: false,
   updateTextId: newTextId => set(() => ({ textId: newTextId })),
+  toggleSidebarEdit: toggleSidebarEdit =>
+    set(() => ({ sidebarEdit: toggleSidebarEdit })),
+  toggleMainAreaEdit: toggleMainAreaEditEdit =>
+    set(() => ({ mainAreaEdit: toggleMainAreaEditEdit })),
 }));
