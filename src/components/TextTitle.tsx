91353dbf207083e68bef050b4e5f08f03420c90d
senna-lang <asrsena3302@gmail.com>
2024-03-10 14:12:58 +0900

    タイトルの編集機能を追加

--- src/components/TextTitle.tsx
+++ src/components/TextTitle.tsx
@@ -1,18 +1,23 @@
 'use client';
 
 import { useStore } from '@/common/store/store';
-import MainAreaEditButton from './MainAreaEditButton';
+import TitleEditButton from './TitleEditButton';
 
 const TextTitle = ({ textTitle }: { textTitle: string }) => {
-  const { mainAreaEdit } = useStore();
+  const { titleEdit, updateTitle } = useStore();
   return (
     <div className="mb-8 flex items-center justify-between">
-      {mainAreaEdit ? (
-        <input type="text" defaultValue={textTitle} />
+      {titleEdit ? (
+        <input
+          type="text"
+          className="text-2xl font-bold "
+          defaultValue={textTitle}
+          onChange={e => updateTitle(e.target.value)}
+        />
       ) : (
         <h1 className="text-2xl font-bold ">{textTitle}</h1>
       )}
-      <MainAreaEditButton />
+      <TitleEditButton />
     </div>
   );
 };
