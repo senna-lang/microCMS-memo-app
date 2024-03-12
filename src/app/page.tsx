b7a43cebd91927aa795cc81300ba45d461e51ba4
senna-lang <asrsena3302@gmail.com>
2024-03-12 17:59:26 +0900

    リファクタ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-import MainArea from '@/components/layouts/MainArea';
-import SideBar from '@/components/layouts/SideBar';
+"use client";
+import MainArea from "@/components/layouts/MainArea";
+import SideBar from "@/components/layouts/SideBar";
 
 const Home = () => {
   return (
     <div>
-      <div className="h-screen items-center justify-center bg-slate-300 md:flex">
-        <div className="h-full md:flex xl:w-[1280px]">
-          <div className="max-h-full border-r md:w-1/4">
+      <div className="flex h-screen items-center justify-center bg-slate-300">
+        <div className="flex h-full w-[1280px]">
+          <div className="max-h-full w-1/4 border-r">
             <SideBar />
           </div>
-          <div className="max-h-screen md:w-3/4">
+          <div className="max-h-screen w-3/4">
             <MainArea />
           </div>
         </div>
