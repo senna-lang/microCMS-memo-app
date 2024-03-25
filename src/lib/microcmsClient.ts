ed48e1fdf7b1530041028d2a287ae1b0a0998f98
senna-lang <asrsena3302@gmail.com>
2024-03-25 18:07:07 +0900

    microCMS初期化

---
+++ src/lib/microcmsClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: "fnblt2o36e",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+});
+
+export const getAllMemo = async () => {
+  const data = await client.get({ endpoint: "memo" });
+  return data;
+};
