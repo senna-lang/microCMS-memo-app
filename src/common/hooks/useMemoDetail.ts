1bfc233305749641e7c1394ad9300def4fe4628e
senna-lang <asrsena3302@gmail.com>
2024-03-30 19:04:37 +0900

    idを依存関係に登録

--- src/common/hooks/useMemoDetail.ts
+++ src/common/hooks/useMemoDetail.ts
@@ -1,18 +1,19 @@
 import { instance } from "@/lib/axiosClient";
 import { useQuery } from "@tanstack/react-query";
 
-const fetcher = async (id:string) => {
+const fetcher = async (id: string) => {
   const res = await instance.get(`/api/memoContent/${id}`);
   return res.data;
 };
 
-export const useMemoDetail = (id:string) => {
+export const useMemoDetail = (id: string) => {
   const { data: memoContent, status } = useQuery({
-    queryKey: ["memoDetail"],
+    queryKey: ["memoDetail", id],
     queryFn: async () => {
-      const data = await fetcher(id)
-      return data
+      const data = await fetcher(id);
+      return data;
     },
+    enabled: !!id,
   });
 
   return {
