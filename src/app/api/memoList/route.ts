a46e111c3af1639330983873610d91b57af66f29
senna-lang <asrsena3302@gmail.com>
2024-03-30 19:03:49 +0900

    小さな修正

--- src/app/api/memoList/route.ts
+++ src/app/api/memoList/route.ts
@@ -1,5 +1,6 @@
 import { getAllMemo } from "@/lib/microcmsClient";
 import { NextResponse } from "next/server";
+
 export async function GET(req: Request) {
   const res = await getAllMemo();
   return NextResponse.json(res);
