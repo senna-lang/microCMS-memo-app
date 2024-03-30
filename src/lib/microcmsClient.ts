b19a683822e6ac7fbca6be7711139d92ab5e73a8
senna-lang <asrsena3302@gmail.com>
2024-03-30 19:07:07 +0900

    add createMemo

--- src/lib/microcmsClient.ts
+++ src/lib/microcmsClient.ts
@@ -1,4 +1,6 @@
+import { PostMemo } from "@/common/types/types";
 import { createClient } from "microcms-js-sdk";
+
 
 export const client = createClient({
   serviceDomain: "fnblt2o36e",
@@ -11,5 +13,16 @@ export const getAllMemo = async () => {
 };
 export const getMemoDetail = async (id: string) => {
   const data = await client.get({ endpoint: "memo", contentId: `${id}` });
+  return data;
+};
+export const createMemo = async (props: PostMemo) => {
+  const { title, content } = props;
+  const data = await client.create({
+    endpoint: "memo",
+    content: {
+      title,
+      content,
+    },
+  });
   return data;
 };
